c85629a5c1b3c3a782a984b3001a041c86e44e90
Eric Chang <efrochang@gmail.com>
2018-04-23 12:42:05 -0500

    Security Config to use DataSource

--- src/main/java/com/springsecurity/config/DemoSecurityConfig.java
+++ src/main/java/com/springsecurity/config/DemoSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.springsecurity.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,30 +15,20 @@ import org.springframework.security.core.userdetails.User.UserBuilder;
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	// [KEY]: DataSource Injection!
+	@Autowired
+	private DataSource securityDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		// In memory authentication
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication()
-			.withUser(users	.username("john")
-							.password("test123")
-							.roles("EMPLOYEE", "CLEANER"))
-			.withUser(users	.username("mary")
-							.password("test123")
-							.roles("EMPLOYEE", "MANAGER"))
-			.withUser(users	.username("susan")
-							.password("test123")
-							.roles("EMPLOYEE", "ADMIN"));
+		auth.jdbcAuthentication().dataSource(securityDataSource);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-			// .anyRequest()
-			// .authenticated()
 			.antMatchers("/")
 			.hasRole("EMPLOYEE")
 			.antMatchers("/leaders/**")
